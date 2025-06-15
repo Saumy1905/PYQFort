5dacbc4a631eada7be543ee12de44f51a8da5d71
Rajveer* <rajveer.24bcs10404@sst.scaler.com>
2025-06-15 18:34:38 +0530

    Added syllabus and PYQs for PPS (ESC-103) - Task 1.1.2

--- create_structure.sh
+++ create_structure.sh
@@ -3,11 +3,14 @@
 # Create base directories
 mkdir -p "college-name-here/01. Computer Engineering/1st Sem/PPS (ESC-103)"
 
-# Navigate to the subject folder
+# Add syllabus file in the right place
+touch "college-name-here/01. Computer Engineering/CE_Syllabus.pdf"
+
+# Navigate to the subject folder for PYQs
 cd "college-name-here/01. Computer Engineering/1st Sem/PPS (ESC-103)"
 
 # Create 16 placeholder PYQs from 2015 to 2022
 for year in {2015..2022}; do
-  touch "PPS (May $year) (ESC-103).pdf"
-  touch "PPS (Dec $year) (ESC-103).pdf"
+    touch "PPS (May $year) (ESC-103).pdf"
+    touch "PPS (Dec $year) (ESC-103).pdf"
 done
